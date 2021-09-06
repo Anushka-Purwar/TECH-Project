f97225842d406fe0ce1d54b6fe7dfa6a2f1039b6
Purwar-Mansi <86889041+Purwar-Mansi@users.noreply.github.com>
2021-09-06 21:38:11 +0530

    Delete admin.py

--- Task 9 and 10/django_portfolio-main/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from blog.models import BlogPost
-from django.contrib import admin
-from .models import BlogPost, Tag
-
-# Register your models here.
-admin.site.register(BlogPost)
-admin.site.register(Tag)
